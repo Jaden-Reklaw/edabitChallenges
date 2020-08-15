efe4e569873e7329f2cb8756ce35f4393bbc7169
Jordan Walker <jwalker117@gmail.com>
2020-08-14 19:56:17 -0500

    adding clone.js

---
+++ Hard/convert.js
@@ -0,0 +1,28 @@
+/*
+Temperature Converter
+Create a function that converts Celcius to Fahrenheit and vice versa.
+
+Examples
+convert("35°C") ➞ "95°F"
+
+convert("19°F") ➞ "-7°C"
+
+convert("33") ➞ "Error"
+Notes
+Round to the nearest integer.
+If the input is incorrect, return "Error".
+For the formulae to convert back and forth, check the Resources tab.
+*/
+
+const convert = (deg) => {
+    console.log(deg.split(''));
+
+    if(deg[deg.length - 1]
+
+    //Celsius to Fahrenheit
+    //C * 9/5 + 32 = F
+
+    //Fahrenheit to Celsius
+    //(F -32) * 5/9 = C
+
+}
---
+++ Medium/clone.js
@@ -0,0 +1,20 @@
+/*
+Clone a List
+The Code tab has a code which attempts to add a clone of an array to itself. There is no error message, but the results are not as expected. Can you fix the code?
+
+Examples
+clone([1, 1]) ➞ [1, 1, [1, 1]]
+
+clone([1, 2, 3]) ➞ [1, 2, 3, [1, 2, 3]]
+
+clone(["x", "y"]) ➞ ["x", "y", ["x", "y"]]
+*/
+
+//my Answer
+let clone = (arr) => [...arr, arr];
+
+//Other solution
+function clone(arr) {
+	arr.push(arr.slice(0))
+	return arr
+}
