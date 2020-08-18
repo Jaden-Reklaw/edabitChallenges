5bde8b37c9a84f16b4efc29cef2174754651f4ab
Jordan Walker <jwalker117@gmail.com>
2020-08-17 23:53:40 -0500

    removeDups.js

---
+++ Medium/removeDups.js
@@ -0,0 +1,18 @@
+/*
+Remove Duplicates from an Array
+Create a function that takes an array of items, 
+removes all duplicate items and returns a new 
+array in the same sequential order as the old 
+array (minus duplicates).
+
+Examples
+removeDups([1, 0, 1, 0]) ➞ [1, 0]
+
+removeDups(["The", "big", "cat"]) ➞ ["The", "big", "cat"]
+
+removeDups(["John", "Taylor", "John"]) ➞ ["John", "Taylor"]
+Notes
+Tests contain arrays with both strings and numbers.
+Tests are case sensitive.
+*/
+const removeDups = arr => [...new Set(arr)];
